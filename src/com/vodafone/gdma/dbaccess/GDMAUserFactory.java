96d62f947671b1e5119a75d83bf8203a5275e49e
soconnell <soconnell@mycompany.com>
2006-03-06 13:41:37 +0000

    SOCO:CR:06: User Maintenance - database class for buling a list of users, editing/deleting/inserting...
    git-svn-id: http://10.252.222.196/svn/GDMA/trunk@20 3272a863-80ad-44bb-9a36-f6e59ebc37f4

---
+++ src/com/vodafone/gdma/dbaccess/GDMAUserFactory.java
@@ -0,0 +1,246 @@
+package com.vodafone.gdma.dbaccess;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author SOCONNELL
+ * 
+ * This class is used to create and retrieve USER Permissions records in the database.
+ * For now, the code only works with an oracle database.
+ * 
+ */
+public class GDMAUserFactory extends DBFactory {
+
+    private static GDMAUserFactory instance = null;
+
+    // Log4j logger
+    private static Logger logger = Logger.getLogger(GDMAUserFactory.class);
+
+    public GDMAUser getGDMAUser(Long id) {
+        GDMAUser user = null;
+        
+        if(id == null)
+            return null;
+        
+        for (int i = 0; i <= list.size(); i++) {
+        	user = (GDMAUser) list.get(i);
+            if (id.equals(user.getId())) return user;
+        }
+        return null;
+    }
+    
+    public GDMAUserFactory() throws Exception {
+        buildList();
+    }
+
+    /*
+     * Build a list of users in the database 
+     * 
+     * (non-Javadoc)
+     * @see com.vodafone.gdma.dbaccess.DBFactory#buildList()
+     */
+    public synchronized void buildList() throws ClassNotFoundException,
+            SQLException, IOException, Exception {
+    	
+    	 // Create the ArrayList which will hold records
+        list = new ArrayList();
+        
+    	 // Create the TreeMap whcih will hold the USERS
+        java.sql.Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        String query = "select * from GDMA_USERS order by USERNAME";
+        // Create the ArrayList which will hold the users
+        list = new ArrayList();
+
+        try {
+            con = DBUtil.getConnection();
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+
+            while (rs != null && rs.next()) {
+                GDMAUser user = new GDMAUser();
+
+                /* CREATE TABLE GDMA.GDMA_USERS 
+                 * (
+                 *     USER_ID    NUMBER(9)     NOT NULL,
+                 *     FIRST_NAME VARCHAR2(255)     NULL,
+                 *     LAST_NAME  VARCHAR2(255)     NULL,
+                 *     USERNAME   VARCHAR2(255) NOT NULL
+                 *  )                  
+                 */
+                user.setId(new Long(rs.getLong("USER_ID")));
+                user.setFirstName(rs.getString("FIRST_NAME"));
+                user.setLastName(rs.getString("LAST_NAME"));
+                user.setUserName(rs.getString("USERNAME"));
+                                
+                list.add(user);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(),e);
+            throw e;
+        } finally {
+            closeAll(con, stmt, rs);
+        }        		 
+    }
+    
+    /*
+     * Method to check is a username is unique
+     * (not checking same user i.e. update)
+     */
+    public boolean userNameUnique(GDMAUser userTest)
+    {
+    	String nameTest = userTest.getUserName();
+    	Long idTest = userTest.getId();
+    	
+    	for(int i=0; i<list.size(); i++)
+    	{
+    	   GDMAUser user = (GDMAUser)list.get(i);
+    	   if(user.getUserName().equalsIgnoreCase(nameTest.trim()))
+    	   {   
+        	   if(user.getId().equals(idTest)) 
+    		   {
+        		   // Logins are the same BUT we are comparing the same USER i.e. AN UPDATE
+        		   // TEST OK
+        		   return true; 
+    		   }
+        	   else
+        	   { 
+        		   // Logins are the same for a NEW USER
+        		   // TEST NOT OK
+        		   return false;
+        	   }
+    	   }
+    	}    	
+        return true;	
+    }
+    
+
+    /*
+     * INSERT a GDMAUser into GDMA_USERS
+     * 
+     */
+    public void addGDMAUser(GDMAUser user) throws Exception 
+    {    	
+    	if(user.getId()!=null)
+    	{
+    		updateGDMAUser(user);
+    	}
+    	else 
+    	{  
+	        Connection con = null;
+	        Statement stmt = null;
+	        StringBuffer sbInsert = new StringBuffer();
+	
+	        sbInsert.append("INSERT INTO GDMA_USERS  ");
+	        sbInsert.append("(FIRST_NAME, LAST_NAME, USERNAME) VALUES (");
+	        sbInsert.append("'");
+	        sbInsert.append(escapeSpecials(user.getFirstName()));
+	        sbInsert.append("','");
+	        sbInsert.append(escapeSpecials(user.getLastName()));
+	        sbInsert.append("','");
+	        sbInsert.append(escapeSpecials(user.getUserName()));
+	        sbInsert.append("')");
+		        
+	        try 
+	        {
+	            con = DBUtil.getConnection();
+	            stmt = con.createStatement();
+	            stmt.executeUpdate(sbInsert.toString());
+	        } 
+	        catch (Exception e) 
+	        {
+	            logger.error(e.getMessage(),e);
+	            throw e;
+	        } 
+	        finally 
+	        {
+	            closeAll(con, stmt, null);
+	        }
+    	}
+    }
+    
+    /*
+     * escapeSpecials for SQL String like APOSTROPHE
+     * 
+     */
+    public String escapeSpecials(String escapeThis) throws Exception 
+    {
+        return escapeThis.replaceAll("'", "''");
+    	
+    }
+    
+    /*
+     * <p>UPDATE a GDMAUser in GDMA_USERS
+     * update GDMA_USERS set  FIRST_NAME='...', LAST_NAME='...', USERNAME='..' where USER_ID...
+     */
+    public void updateGDMAUser(GDMAUser user) throws Exception 
+    {
+    	
+        Connection con = null;
+        Statement stmt = null;
+        StringBuffer sbUpdate = new StringBuffer();
+        sbUpdate.append("update GDMA_USERS set ");
+        sbUpdate.append(" FIRST_NAME='");
+        sbUpdate.append(user.getFirstName());
+        sbUpdate.append("',");
+        sbUpdate.append(" LAST_NAME='");
+        sbUpdate.append(user.getLastName());
+        sbUpdate.append("',");
+        sbUpdate.append(" USERNAME='");
+        sbUpdate.append(user.getUserName());
+        sbUpdate.append("'");
+        sbUpdate.append(" where USER_ID=");
+        sbUpdate.append(user.getId());
+        
+        try {
+            con = DBUtil.getConnection();
+            stmt = con.createStatement();
+            stmt.executeUpdate(sbUpdate.toString());
+        } catch (Exception e) {
+            logger.error(e.getMessage(),e);
+            throw e;
+        } finally {
+            con.commit(); 
+            closeAll(con, stmt, null);
+        }
+    
+    }
+    
+    /*
+     * DELETE a GDMAUser in GDMA_USERS
+     * DELETE FROM GDMA_USERS WHERE USER_ID= ...
+     * 
+     */
+    public void deleteGDMAUser(GDMAUser user) throws Exception 
+    {
+    	
+
+        Connection con = null;
+        Statement stmt = null;
+        StringBuffer sbDelete = new StringBuffer();
+        
+        sbDelete.append("DELETE FROM GDMA_USERS WHERE USER_ID=");
+        sbDelete.append(user.getId());
+
+        try {
+            con = DBUtil.getConnection();
+            stmt = con.createStatement();
+            stmt.executeUpdate(sbDelete.toString());
+        } catch (Exception e) {
+            logger.error(e.getMessage(),e);
+            throw e;
+        } finally {
+            con.commit(); 
+            closeAll(con, stmt, null);
+        }
+    
+    }
+}
