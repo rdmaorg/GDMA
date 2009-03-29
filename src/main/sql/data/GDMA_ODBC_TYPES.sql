cba41683e4b3c6e9be33d662877ea7ced1e83730
svu <svu@mycompany.com>
2009-03-29 00:18:54 +0000

    improved naming, commenting out ODBC as non-x-platform
    git-svn-id: http://10.252.222.196/svn/GDMA/trunk@62 3272a863-80ad-44bb-9a36-f6e59ebc37f4

--- src/main/sql/data/GDMA_ODBC_TYPES.sql
+++ src/main/sql/data/GDMA_ODBC_TYPES.sql
@@ -1,16 +1,18 @@
 delete from GDMA2_ODBC_TYPES;
 commit;
+-- insert into GDMA2_ODBC_TYPES (ID, NAME, SELECT_GET_TABLES, CONNECTIONCLASS)
+-- values (1, 'MS SQL Server (ODBC)', 'SELECT NAME as TABLE_NAME FROM SYSOBJECTS WHERE XTYPE=''U'' ORDER BY NAME', 'sun.jdbc.odbc.JdbcOdbcDriver');
 insert into GDMA2_ODBC_TYPES (ID, NAME, SELECT_GET_TABLES, CONNECTIONCLASS)
-values (1, 'MS SQL Server 2000', 'SELECT NAME as TABLE_NAME FROM SYSOBJECTS WHERE XTYPE=''U'' ORDER BY NAME', 'sun.jdbc.odbc.JdbcOdbcDriver');
+values (1, 'MS SQL Server (JDBC)', 'SELECT NAME as TABLE_NAME FROM SYSOBJECTS WHERE XTYPE=''U'' ORDER BY NAME', 'com.microsoft.sqlserver.jdbc.SQLServerDriver');
+-- insert into GDMA2_ODBC_TYPES (ID, NAME, SELECT_GET_TABLES, CONNECTIONCLASS)
+-- values (2, 'Oracle 8/9 (ODBC)', 'select table_name as TABLE_NAME from user_tables', 'sun.jdbc.odbc.JdbcOdbcDriver');
 insert into GDMA2_ODBC_TYPES (ID, NAME, SELECT_GET_TABLES, CONNECTIONCLASS)
-values (2, 'ODBC Oracle 8/9', 'select table_name as TABLE_NAME from user_tables', 'sun.jdbc.odbc.JdbcOdbcDriver');
+values (3, 'Oracle 8/9 (JDBC)', 'select table_name as TABLE_NAME from user_tables', 'oracle.jdbc.OracleDriver');
+-- insert into GDMA2_ODBC_TYPES (ID, NAME, SELECT_GET_TABLES, CONNECTIONCLASS)
+-- values (4, 'Teradata 8/9 (ODBC)', 'SELECT TABLENAME AS TABLE_NAME  FROM DBC.TABLES ORDER BY TABLENAME', 'sun.jdbc.odbc.JdbcOdbcDriver');
 insert into GDMA2_ODBC_TYPES (ID, NAME, SELECT_GET_TABLES, CONNECTIONCLASS)
-values (3, 'JDBC Oracle 8/9', 'select table_name as TABLE_NAME from user_tables', 'oracle.jdbc.OracleDriver');
+values (5, 'Teradata 8/9 (JDBC)', 'SELECT TABLENAME AS TABLE_NAME  FROM DBC.TABLES ORDER BY TABLENAME', 'com.ncr.teradata.TeraDriver');
 insert into GDMA2_ODBC_TYPES (ID, NAME, SELECT_GET_TABLES, CONNECTIONCLASS)
-values (4, 'ODBC Teradata 8/9', 'SELECT TABLENAME AS TABLE_NAME  FROM DBC.TABLES ORDER BY TABLENAME', 'sun.jdbc.odbc.JdbcOdbcDriver');
-insert into GDMA2_ODBC_TYPES (ID, NAME, SELECT_GET_TABLES, CONNECTIONCLASS)
-values (5, 'JDBC Teradata 8/9', 'SELECT TABLENAME AS TABLE_NAME  FROM DBC.TABLES ORDER BY TABLENAME', 'com.ncr.teradata.TeraDriver');
-insert into GDMA2_ODBC_TYPES (ID, NAME, SELECT_GET_TABLES, CONNECTIONCLASS)
-values (6, 'JDBC MySQL', 'SHOW TABLES', 'com.mysql.jdbc.Driver');
+values (6, 'MySQL (JDBC)', 'SHOW TABLES', 'com.mysql.jdbc.Driver');
 commit;
 
