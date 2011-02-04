b2bf7621c71f534e25a2c6099ed9a5cd24cfd158
John Roche <john.roche@clientsolutions.ie>
2011-02-04 10:21:56 +0000

    Enhanced Security changes
    git-svn-id: http://10.252.222.196/svn/GDMA/branches/bulk_import@196 3272a863-80ad-44bb-9a36-f6e59ebc37f4

--- src/main/GDMA.war/js/admin/tables-min.js
+++ src/main/GDMA.war/js/admin/tables-min.js
@@ -1,6 +1,6 @@
 YAHOO.namespace("GDMA.admin.tables");
-YAHOO.GDMA.admin.tables.fields={fields:[{key:"id",parser:YAHOO.util.DataSource.parseNumber},{key:"name",parser:YAHOO.util.DataSource.parseString},{key:"displayed"},{key:"allowDelete"}]};
-YAHOO.GDMA.admin.tables.columnDefs=[{label:"",formatter:YAHOO.GDMA.utilities.rownumFormatter,minWidth:20},{key:"name",label:"Name",sortable:true,resizeable:true,width:200},{key:"displayed",label:YAHOO.GDMA.utilities.createCheckAllHeader("Displayed"),formatter:"checkbox",resizeable:true,width:85},{key:"allowDelete",label:YAHOO.GDMA.utilities.createCheckAllHeader("Allow Delete"),formatter:"checkbox",resizeable:true,width:85}];
+YAHOO.GDMA.admin.tables.fields={fields:[{key:"id",parser:YAHOO.util.DataSource.parseNumber},{key:"name",parser:YAHOO.util.DataSource.parseString}]};
+YAHOO.GDMA.admin.tables.columnDefs=[{label:"",formatter:YAHOO.GDMA.utilities.rownumFormatter,minWidth:20},{key:"name",label:"Name",sortable:true,resizeable:true,width:200}];
 YAHOO.GDMA.admin.tables.doubleClick=function(B){var A=YAHOO.GDMA.admin.dataTable.getTrEl(B.target);
 YAHOO.GDMA.admin.dataTable.selectRow(A);
 YAHOO.GDMA.admin.dataTable.focus();
